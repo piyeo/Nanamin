c234e5e3094c06acd3f4d4ac35d894a2ee85a7de
piyeo <sakana.mo17@gmail.com>
2020-05-08 10:55:41 +0900

    NGワード追加

--- tweet.py
+++ tweet.py
@@ -11,11 +11,15 @@ api = tweepy.API(auth)
 
 try:
     sen_list = []
-    for status in api.home_timeline(count=100):
+    for status in api.home_timeline(count=200):
         sentence = str(status.text).replace('\n', '')
         if len(sentence) <= 50 and len(sentence) >= 10:
-            if "@" not in sentence:
-                if "http" not in sentence or "t.co" in sentence:
+            if "@" not in sentence and "質問" not in sentence and\
+                    "おはよ" not in sentence and "普通" not in sentence and\
+                    "笑" not in sentence and "おやすみ" not in sentence and\
+                    "かわいい" not in sentence and "よろしく" not in sentence and\
+                    "草" not in sentence and "w" not in sentence and "寝" not in sentence:
+                if "http" not in sentence:
                     if len(sentence) >= 30:
                         sentence = sentence[:30]
                     print(sentence)
