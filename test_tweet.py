81bda8ad7198c303f4052c3e50b9b0ae20ccc913
piyeo <sakana.mo17@gmail.com>
2020-05-12 00:10:18 +0900

    NGワードをリスト化

--- test_tweet.py
+++ test_tweet.py
@@ -1,26 +1,11 @@
 import tweepy
 import random
-import json
-import urllib.request
 
 import key
 
-url = 'http://ns.dena.jp/mbga/gameapi//v1/ngword?_method=check&format=json'
-data = {
-    'data': "「殺す」はNGワードであるか",
-}
-headers = {
-    'Content-Type': 'application/json',
-}
+auth = tweepy.OAuthHandler(key.CK, key.CS)
+auth.set_access_token(key.AT, key.AS)
 
-req = urllib.request.Request(url, json.dumps(data).encode(), headers)
-with urllib.request.urlopen(req) as res:
-    body = res.read()
-    print(body)
+api = tweepy.API(auth)
 
-# auth = tweepy.OAuthHandler(key.CK, key.CS)
-# auth.set_access_token(key.AT, key.AS)
-#
-# api = tweepy.API(auth)
-
-# api.update_status("今から運動してきますだいたい5キロか6キロくらい走るのって普通……だよね？")
+api.update_status("")
